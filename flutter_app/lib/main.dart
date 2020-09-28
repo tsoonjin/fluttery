9ec1eefd68b44730945244793c907a450054d661
jinified <jinified@gmail.com>
2020-09-28 21:37:07 +0800

    Add infinite scroll list

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Baby Name Generator',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       // home: MyHomePage(title: 'Flutter My Demo Home Page'),
-      home: helloWorld
+      home: RandomWords()
     );
   }
 }
