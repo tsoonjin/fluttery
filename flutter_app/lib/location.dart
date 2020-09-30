3a028c98db654831258510182abe8956194f8f66
jinified <jinified@gmail.com>
2020-09-30 21:29:38 +0800

    Complete simple layout akin to map like single page

--- flutter_app/lib/location.dart
+++ flutter_app/lib/location.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
+Widget textSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
+
 Widget titleSection = Container(
   padding: const EdgeInsets.all(32),
   child: Row(
@@ -38,12 +51,58 @@ Widget titleSection = Container(
   ),
 );
 
-var location = Scaffold(
-  appBar: AppBar(title: Text('Flutter layout demo')),
-  body: Column(
+Widget btnSection(Color color) {
+  return Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildBtnColumn(color, Icons.call, 'CALL'),
+        _buildBtnColumn(color, Icons.near_me, 'ROUTE'),
+        _buildBtnColumn(color, Icons.share, 'SHARE'),
+      ]
+    )
+  );
+}
+
+Column _buildBtnColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      titleSection
+      Icon(icon, color: color),
+      Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: color
+          )
+        )
+      )
     ]
-  )
-);
+  );
+}
+
+Widget location(BuildContext context) {
+  return Scaffold(
+      appBar: AppBar(title: Text('Map')),
+      body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 12),
+              child:
+              Image.network(
+                  'https://picsum.photos/250?image=9',
+                  width: 600,
+                  height: 240
+              )
+            ),
+            titleSection,
+            btnSection(Theme.of(context).primaryColor),
+            textSection
+          ]
+      )
+  );
+}
 
